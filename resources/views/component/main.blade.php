82ac251ab05ce38f52790897787d66b6867a4616
Wahyu Kristiawan <whykris97@gmail.com>
2022-08-22 01:51:38 +0700

    init

---
+++ resources/views/component/main.blade.php
@@ -0,0 +1,19 @@
+<main id="main">
+
+    @include('section.featured')
+
+    @include('section.about')
+
+    @include('section.whyus')
+
+    @include('section.client')
+
+    @include('section.service')
+
+    @include('section.portfolio')
+
+    @include('section.team')
+
+    @include('section.contact')
+
+</main><!-- End #main -->
